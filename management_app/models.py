ed1780435a088966ab4fa3d5222ed8c01ef93b73
Alkama Umar Liman <alkalineumarliman@gmail.com>
2026-04-21 17:35:57 +0100

    I add defartment in order to help me group my timetable easily

--- management_app/models.py
+++ management_app/models.py
@@ -161,9 +161,8 @@ class Timetable(models.Model):
     lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE, related_name='schedules')
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='schedules')
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE, related_name='schedules')  # ✅ Added semester context
-    depertment = models.ForeignKey(Department,on_delete=models.CASCADE)
+    department = models.ForeignKey(Department,on_delete=models.CASCADE)
     date = models.DateField(default=date.today)
-    
     day = models.CharField(max_length=3, choices=DAY_CHOICES)
     level = models.ForeignKey(Level,on_delete=models.SET_NULL,null=True)
     start_time = models.TimeField()
